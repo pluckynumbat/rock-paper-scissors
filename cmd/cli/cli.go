d0a54e552156097a5338bc84969d7b862e95a9f6
Neehar Patil <pluckynumbat@gmail.com>
2024-10-10 11:10:39 +0530

    added a basic command line interface that can currently run a fixed input game of rock paper scissors

---
+++ cmd/cli/cli.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pluckynumbat/rock-paper-scissors/engine"
+)
+
+func main() {
+	fmt.Println("Let's play rock paper scissors!")
+
+	player1 := engine.Player{Name: "Player 1"}
+	player2 := new(engine.Player)
+	player2.Name = "Player 2"
+
+	for done := false; done != true; {
+		fmt.Println("For a fixed choice game, press 1")
+		fmt.Println("For a random choice game, press 2")
+		fmt.Println("Press any other key to quit...")
+
+		var option string
+		_, err := fmt.Scanln(&option)
+		if err != nil {
+			fmt.Println(fmt.Errorf("input failed with %v", err))
+			break
+		}
+
+		switch option {
+		case "1":
+			runFixedChoice(&player1, player2)
+
+		case "2":
+			runRandomChoice(&player1, player2)
+
+		default:
+			done = true
+		}
+	}
+}
+
+func runFixedChoice(p1, p2 *engine.Player) {
+
+	var input1, input2 string
+	var choice1, choice2 engine.Choice
+	var choiceErr error
+
+	fmt.Printf("Enter choice for %s: ", p1)
+
+	for {
+		_, err := fmt.Scanln(&input1)
+		if err != nil {
+			fmt.Println(fmt.Errorf("input failed with %v", err))
+			break
+		}
+		choice1, choiceErr = getChoiceFromInput(input1)
+		if choiceErr != nil {
+			fmt.Println(fmt.Errorf("choice assignment failed on: %v", choiceErr))
+			fmt.Println("Please try again...")
+		} else {
+			break
+		}
+	}
+	p1.ChooseFixed(choice1)
+	fmt.Println(p1, "chose:", p1.PrintChoice())
+
+	fmt.Printf("Enter choice for %s: ", p2)
+
+	for {
+		_, err := fmt.Scanln(&input2)
+		if err != nil {
+			fmt.Println(fmt.Errorf("input failed with %v", err))
+			break
+		}
+		choice2, choiceErr = getChoiceFromInput(input2)
+		if choiceErr != nil {
+			fmt.Println(fmt.Errorf("choice assignment failed on: %v", choiceErr))
+			fmt.Println("Please try again...")
+		} else {
+			break
+		}
+	}
+	p2.ChooseFixed(choice2)
+	fmt.Println(p2, "chose:", p2.PrintChoice())
+
+	winner, err := engine.Play(p1, p2)
+	if err != nil {
+		fmt.Printf("Play failed with error: %v \n", err)
+		return
+	}
+	fmt.Println(winner, "Won! \n")
+}
+
+func runRandomChoice(p1, p2 *engine.Player) {
+
+}
+
+func getChoiceFromInput(input string) (engine.Choice, error) {
+
+	switch input {
+	case "R", "r":
+		return engine.Rock, nil
+
+	case "P", "p":
+		return engine.Paper, nil
+
+	case "S", "s":
+		return engine.Scissors, nil
+
+	default:
+		return engine.None, fmt.Errorf("Invalid input")
+	}
+}
