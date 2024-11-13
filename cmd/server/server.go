c4d5cdf4e1abd5b84a461a590f47ed7371a01101
Neehar Patil <pluckynumbat@gmail.com>
2024-11-13 13:50:53 +0530

    create 2 players, choose random choices, print choices

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/pluckynumbat/rock-paper-scissors/engine"
 )
 
 func main() {
 	fmt.Println("running the rock-paper-server...")
+	playRandomGame()
+}
+
+func playRandomGame() {
+	p1 := &engine.Player{Name: "Player 1"}
+	p1.ChooseRandom()
+	fmt.Printf("%v chose %v\n", p1.String(), p1.PrintChoice())
+
+	p2 := &engine.Player{Name: "Player 2"}
+	p2.ChooseRandom()
+	fmt.Printf("%v chose %v\n", p2.String(), p2.PrintChoice())
 }
