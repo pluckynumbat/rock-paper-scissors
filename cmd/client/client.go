45456f41e255997d5c9b58615fb3869b35d9c4d5
Neehar Patil <pluckynumbat@gmail.com>
2024-11-15 14:16:15 +0530

    make a request for a random game to the localhost server and print the response status

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("welcome to the rock-paper-client...")
+
+	resp, err := http.Get("http://localhost:8080/random")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status: ", resp.Status)
 }
