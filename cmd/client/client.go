ff58ec10c460f155e641b24899d886eb991e4f9d
Neehar Patil <pluckynumbat@gmail.com>
2024-11-15 14:20:37 +0530

    get the server url from the standard input

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -9,6 +9,15 @@ import (
 func main() {
 	fmt.Println("welcome to the rock-paper-client...")
 
+	fmt.Println("Please enter the server URL: ")
+
+	serverURL := ""
+	_, err := fmt.Scanln(&serverURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
 	resp, err := http.Get("http://localhost:8080/random")
 	if err != nil {
 		fmt.Println("Error: ", err)
