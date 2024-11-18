9b4496225a9b5b4b48c5fa202286f4d3fc2a8ac4
Neehar Patil <pluckynumbat@gmail.com>
2024-11-18 16:45:46 +0530

    remove obsolete play random game function

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -20,7 +20,7 @@ func (gs *GameServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	var err error
 	endpoint := req.URL.Path
 	switch endpoint {
-	case "/random":
+	case "/play-random":
 		result, err = playRandomAgainstServer(serverPlayer)
 	case "/play-rock":
 		result, err = playRockAgainstServer(serverPlayer)
@@ -40,29 +40,6 @@ func (gs *GameServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 	fmt.Println("running the rock-paper-server...")
 	log.Fatal(http.ListenAndServe(":8080", &GameServer{}))
-}
-
-func playRandomGame() (string, error) {
-	result := ""
-
-	p1 := createPlayerWithRandomChoice("Player 1")
-	fmt.Printf(" %v:%v ", p1.String(), p1.PrintChoice())
-	result += fmt.Sprintf("%v chose %v\n", p1.String(), p1.PrintChoice())
-
-	p2 := createPlayerWithRandomChoice("Player 2")
-	fmt.Printf(" %v:%v ", p2.String(), p2.PrintChoice())
-	result += fmt.Sprintf("%v chose %v\n", p2.String(), p2.PrintChoice())
-
-	win, err := engine.Play(p1, p2)
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-		return "", err
-	} else {
-		fmt.Printf(" Win: %v\n", win.String())
-		result += fmt.Sprintf("%v Won!\n", win.String())
-	}
-
-	return result, nil
 }
 
 func playRandomAgainstServer(serverPlayer *engine.Player) (string, error) {
