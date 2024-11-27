28fcf945bcdc06bd84f15a47226dddcf1a4538cb
Neehar Patil <pluckynumbat@gmail.com>
2024-11-27 13:13:22 +0530

    added a test for the create server url prefix function

--- cmd/client/client_test.go
+++ cmd/client/client_test.go
@@ -24,6 +24,30 @@ func randomResult(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Random Result!")
 }
 
+func TestCreateServerURLPrefix(t *testing.T) {
+	var tests = []struct {
+		name       string
+		baseURL    string
+		portNumber string
+		want       string
+	}{
+		{"blank", "", "", "http://:"},
+		{"local-host", "localhost", "8080", "http://localhost:8080"},
+		{"loop-back", "127.0.0.1", "8080", "http://127.0.0.1:8080"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			got := createServerURLPrefix(test.baseURL, test.portNumber)
+			if got != test.want {
+				t.Errorf("got incorrect results, want: %v, got: %v", test.want, got)
+			}
+		})
+	}
+}
+
+
 func TestPlayRandomRequest(t *testing.T) {
 	newServer := httptest.NewServer(http.HandlerFunc(randomResult))
 
