0c99755a837d3a78d9d53aa15c8f44ade8a18f0e
Neehar Patil <pluckynumbat@gmail.com>
2024-11-27 13:19:29 +0530

    fixed some typos, added a test for the player scissors request

--- cmd/client/client_test.go
+++ cmd/client/client_test.go
@@ -98,7 +98,7 @@ func TestPlayRockRequest(t *testing.T) {
 	}
 }
 
-func TestPlayPaperkRequest(t *testing.T) {
+func TestPlayPaperRequest(t *testing.T) {
 	var tests = []struct {
 		name    string
 		handler func(w http.ResponseWriter, req *http.Request)
@@ -117,7 +117,39 @@ func TestPlayPaperkRequest(t *testing.T) {
 			defer newServer.Close()
 
 			if err != nil {
-				t.Fatalf("sendPlayRockRequest failed. Error: %v", err)
+				t.Fatalf("sendPlayPaperRequest failed. Error: %v", err)
+			}
+
+			want := test.want
+			got := strings.TrimSpace(result)
+
+			if got != want {
+				t.Errorf("got incorrect results, want: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+
+func TestPlayScissorsRequest(t *testing.T) {
+	var tests = []struct {
+		name    string
+		handler func(w http.ResponseWriter, req *http.Request)
+		want    string
+	}{
+		{"server plays rock", serverWins, "Server Wins"},
+		{"server plays paper", youWin, "You Win"},
+		{"server plays scissors", noOneWins, "No One Wins"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			newServer := httptest.NewServer(http.HandlerFunc(test.handler))
+
+			result, err := sendPlayScissorsRequest(newServer.URL)
+			defer newServer.Close()
+
+			if err != nil {
+				t.Fatalf("sendPlayScissorsRequest failed. Error: %v", err)
 			}
 
 			want := test.want
